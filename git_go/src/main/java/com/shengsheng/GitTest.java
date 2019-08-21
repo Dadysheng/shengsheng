b1b070cb5156461023a3ebf1163a4763bf564050
Dadysheng <1844788196@qq.com>
2019-08-21 21:38:41 +0800

    第一次由idea提交到本地仓库

--- git_go/src/main/java/com/shengsheng/GitTest.java
+++ git_go/src/main/java/com/shengsheng/GitTest.java
@@ -3,5 +3,6 @@ package com.shengsheng;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("I love  you");
+        System.out.println("hello_git");
     }
 }
